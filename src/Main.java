9e2f769bea36c5ec78dcaf083ef28628a844b3f3
Diego Oloarte <diegooloarte269@gmail.com>
2022-11-04 19:14:19 -0600

    Versión con autómatas

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,21 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.stream.Collectors;
 
 import AnalizadorLexico.AnalizadorLexico;
+import AnalizadorLexico.TablaSimbolos;
 
 public class Main {
 
     static File file; 
     static int numLinea = 0;
     static BufferedReader br;
+    static TablaSimbolos ts; 
 
     public static void main(String [] args) throws Exception {
+
+        llenarTabSim();
 
         if (!abrirArchivo()) 
             return;
@@ -23,7 +28,7 @@ public class Main {
     public static boolean abrirArchivo() {
         try {
             // args[0] = "C:/Users/diieg/OneDrive - Instituto Politecnico Nacional/ESCOM/1ER SEMESTRE/Fundamentos de Programación/1er Parcial/Práctica 4/Ejercicio 1/ejercicio1.c";
-            file = new File("C:/Users/diieg/OneDrive - Instituto Politecnico Nacional/ESCOM/1ER SEMESTRE/Fundamentos de Programación/1er Parcial/Práctica 4/Ejercicio 1/ejercicio1.c");
+            file = new File("C:/Users/Diego Oloarte/Desktop/hola.c");
             br = new BufferedReader(new FileReader(file));
             
         } catch (FileNotFoundException e) {
@@ -35,37 +40,39 @@ public class Main {
     }
 
     public static void recorrerArchivo() {
-        String linea;
-        int numLinea = 0;
-        
+
         AnalizadorLexico al = new AnalizadorLexico();
 
         try {
-            // Recorre todo el archivo completo
-            while ((linea = br.readLine()) != null) {
+            String siguienteToken;
 
-                al.setLinea(linea.trim()+'\n');
-                String siguienteToken;
+            // Le pasamos el código fuente al analizador léxico
+            al.setCodigoFuente(br.lines().collect(Collectors.joining()) + '\0');
 
-                // Recorre la linea en cuestión 
-                do {
-                    siguienteToken = al.siguienteToken();
-                    System.out.println(siguienteToken);
+            do {
+                siguienteToken = al.siguienteToken();
+                System.out.println(siguienteToken);
+            } while (siguienteToken != "");
 
-                    //if(siguienteToken matches with reserved words regex...)
-                        //agregar a la tabla de símbolos como palabra reservada
-                    //else if(//siguienteToken matches with identifiers regex...)
-                        //agregar a la tabla de símbolos como identificador 
-                }while(!al.getFinDeLinea());
 
-                al.setFinDeLinea(false);
-            }
+            
             
             br.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
+        }       
+    }
+
+    public static void llenarTabSim() {
+        String [] palabrasReservadas = {"auto", "break", "case", "char", "const", "continue", 
+                                        "default", "do", "double", "else", "enum", "extern", 
+                                        "float", "for", "goto", "if", "int", "long", "main", 
+                                        "register", "return", "short", "signed", "sizeof", 
+                                        "static", "struct", "switch", "typedef", "union", 
+                                        "unsigned", "void", "volatile", "while",  "_Packed", 
+                                    };
+        for (String palabraReservada : palabrasReservadas) {
+            ts.agregarPR(palabraReservada);
         }
-        
-        
     }
 }
--- src/TablaSimbolos.java
+++ src/TablaSimbolos.java
@@ -4,7 +4,7 @@ import java.util.Hashtable;
 
 public class TablaSimbolos extends Hashtable<String, ArrayList<Integer>>{
 
-    public void agregarNuevo(String lexema, int numLinea) {
+    public void agregarIdentificador(String lexema, int numLinea) {
 
         ArrayList <Integer> lineas = new ArrayList<>();
         lineas.add(numLinea);
@@ -16,4 +16,17 @@ public class TablaSimbolos extends Hashtable<String, ArrayList<Integer>>{
         lineas.add(numLinea);
         this.put(lexema, lineas);
     }
+
+    public void agregarPR(String lexema) {
+        this.put(lexema, new ArrayList<Integer>());
+    }
 }
+
+// public Simbolo get(String s) {
+//     for( Ent e = this; e != null; e = e.ant ) {
+//     Simbolo encontro = (Simbolo)(e.tabla.get(s));
+//     if( encontro != null ) return encontro;
+//     }
+//         return null;
+//     }
+// }
